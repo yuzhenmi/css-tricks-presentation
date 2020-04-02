159d1d87feae76286473fad257ede5a6efafc2f8
Hans Yu <hansyuhans@gmail.com>
2020-04-01 23:37:54 -0400

    Updates

---
+++ precache-manifest.0e47a58812346989a7d72a1988a77bc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a02e2a06c71cd9babaeb225f80d918",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1373970b5629dc275dcd",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d28323f81818232bafe1",
+    "url": "/static/js/2.af0ad3d0.chunk.js"
+  },
+  {
+    "revision": "f74a6debb7da89320918198b818026f8",
+    "url": "/static/js/2.af0ad3d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1373970b5629dc275dcd",
+    "url": "/static/js/main.50246194.chunk.js"
+  },
+  {
+    "revision": "cad81f0105241ca3d3f2",
+    "url": "/static/js/runtime-main.5ee96901.js"
+  },
+  {
+    "revision": "8c0faa9bbdb168410e940a0d8323bf63",
+    "url": "/static/media/angel.8c0faa9b.png"
+  },
+  {
+    "revision": "ddef0d0b185e25263e65ecbdbab75af2",
+    "url": "/static/media/wing.ddef0d0b.png"
+  }
+]);
